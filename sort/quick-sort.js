aa1f82437703c9d7e7ad9b254007b233f3fd6008
sukuangxie <sukuangxie@tencent.com>
2022-09-18 23:50:35 +0800

    feat: add comment

--- sort/quick-sort.js
+++ sort/quick-sort.js
@@ -36,6 +36,9 @@ function quickSortImplement(list, left, right) {
 function partition(list, left, right) {
   let i = left, boundary = left;
   const targetNum = list[right]; // 先不考虑随机
+  // 参考荷兰国旗题目，需要处理等于的情况，将等于的结果进行交换
+  // 如果出现小于等于哨兵的值，交换，边界和指针移动
+  // 出现大于情况，直接进行移动指针即可
   while (i <= right) {
     if (list[i] <= targetNum) {
       swap(list, i, boundary);
