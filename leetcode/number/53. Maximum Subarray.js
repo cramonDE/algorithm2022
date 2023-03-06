5d2cfd301f1c3e2835844e1d7b4c1c5372ae795e
cramonDE <cramonDE@github.com>
2023-03-06 23:50:29 +0800

    refactor: add comment

--- leetcode/number/53. Maximum Subarray.js
+++ leetcode/number/53. Maximum Subarray.js
@@ -7,20 +7,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
+var maxSubArray = function (nums) {
   const dp = [];
   dp.push(nums[0]);
   let answer = dp[0];
+  // 以 nums[i] 结尾的最大子串，这个 nums[i] 一定会在dp中，就看需不需要加上 dp[i - 1]
   for (let i = 1; i < nums.length; i++) {
-    if (dp[i - 1] < 0) {
-      dp.push(nums[i]);
-    answer = Math.max(answer, nums[i]);
-    } else {
-      dp.push(nums[i] + dp[i - 1]);
-    answer = Math.max(answer, nums[i] + dp[i - 1]);
-
-    }
+    // 如果 dp[i - 1] < 0，加上去肯定变小了，舍弃掉，单独成一个项
+    dp.push(dp[i - 1] < 0 ? nums[i] : nums[i] + dp[i - 1]);
+    answer = Math.max(answer, dp[i]);
   }
+
   return answer;
 };
 
@@ -28,4 +25,4 @@ module.exports = {
   maxSubArray
 };
 
-console.log(maxSubArray([5,4,-1,7,8])); // 23
+console.log(maxSubArray([5, 4, -1, 7, 8])); // 23
